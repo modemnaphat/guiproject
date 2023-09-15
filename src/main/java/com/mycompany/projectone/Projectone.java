1bd7e892b1c1cece1b97eac3dce40a5cc40a19c4
modemnaphat <119996385+modemnaphat@users.noreply.github.com>
2023-09-15 18:27:41 +0700

    netbeans add

---
+++ src/main/java/com/mycompany/projectone/Projectone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectone;
+
+/**
+ *
+ * @author NITRO 5
+ */
+public class Projectone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
